b944a3cca6daa4c2ebd8b7fbd4cefee5c652514d
Tobias Bouschen <tobias.bouschen@googlemail.com>
2020-05-14 14:17:58 +0200

    [DOC][CORE] Add javadoc for resource interfaces
    Adds javadoc for the resource interfaces describing their desired
    behavior.
    
    Some implementations might not match the described behavior. This will
    be corrected in the upcoming migration to using reference points as the
    resource implementations will have to be reworked anyways.

--- core/src/saros/filesystem/IProject.java
+++ core/src/saros/filesystem/IProject.java
@@ -1,28 +1,7 @@
-/*
- *
- *  DPP - Serious Distributed Pair Programming
- *  (c) Freie Universität Berlin - Fachbereich Mathematik und Informatik - 2010
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 1, or (at your option)
- *  any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- * /
- */
-
 package saros.filesystem;
 
 /**
- * This interface is under development. It currently equals its Eclipse counterpart. If not
- * mentioned otherwise all offered methods are equivalent to their Eclipse counterpart.
+ * This interface is under development. It is currently a placeholder and will be reworked once the
+ * migration to reference points is completed.
  */
 public interface IProject extends IContainer {}
